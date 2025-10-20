bcb6c0566f136966fb04beb9784b43dcebc73812
Davi Castro <davifgeo@gmail.com>
2025-10-20 20:33:44 +0000

    Ajustes/Aula i18n (Internacionalização)

--- salesorderts/webapp/controller/View1.controller.ts
+++ salesorderts/webapp/controller/View1.controller.ts
@@ -1,4 +1,9 @@
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
+import Event from "sap/ui/base/Event";
 import Controller from "sap/ui/core/mvc/Controller";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import ODataModel from "sap/ui/model/odata/v2/ODataModel";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
 
 /**
  * @namespace apps.dflc.salesorderts.controller
@@ -8,5 +13,27 @@ export default class View1 extends Controller {
     /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
 
+        var oView = this.getView();
+        var oModel = new JSONModel();
+        oModel.setData({ "usuario": { "nome": "Davi" } });
+        oView?.setModel(oModel);
+
     }
+
+    public onPress(oEvent: Event) {
+        alert("Bral")
+    }
+
+    public onExibirMensagem() {
+
+        var oI18nModel = this.getView()?.getModel("i18n") as ResourceModel;
+        var oI18n = oI18nModel.getResourceBundle() as ResourceBundle;
+        var oModel = this.getView()?.getModel("usuario") as JSONModel;
+        var oData = oModel?.getData();
+
+        var sText = oI18n.getText("welcomeMsg", [oData.usuario.nome]);
+
+        alert(sText);
+    }
+
 }
